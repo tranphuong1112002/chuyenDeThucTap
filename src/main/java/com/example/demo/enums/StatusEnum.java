d21cb2384d16eb95809efd21220efe64065000b6
hungdoviet <hungdoviet@lptech.vn>
2023-11-12 14:14:55 +0700

    update status enum

--- src/main/java/com/example/demo/enums/StatusEnum.java
+++ src/main/java/com/example/demo/enums/StatusEnum.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum StatusEnum {
-  TAO_MOI("TAO_MOI", "Tạo mới"),
+//  TAO_MOI("TAO_MOI", "Tạo mới"),
   DANH_GIA("DANH_GIA", "Đánh giá CV"),
-  LH("LH", "Liên hệ ứng viên"),
+//  LH("LH", "Liên hệ ứng viên"),
   PV("PV", "Phỏng vấn"),
   TV("TV", "Tham vấn lương"),
   OFFER("OFFER", "Nhận offer"),
